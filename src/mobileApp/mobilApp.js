ea24bf72e6a0d8cf09a0a3cf31b79e366c24d5da
Hyelan <hyelanlove@gmail.com>
2023-11-11 13:49:47 +0900

    add installing function

--- src/mobileApp/mobilApp.js
+++ src/mobileApp/mobilApp.js
@@ -1,2 +1,33 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+// const apkDownloadBtn = document.querySelector(".apk")
+
+// function downloadApk() {
+//   const serverUrl = 'http://localhost:3000/download';
+
+//   fetch(serverUrl, {
+//     method: 'GET',
+//   })
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! Status: ${response.status}`);
+//     }
+//     return response.blob();
+//   })
+//   .then(blob => {
+//     const file = new File([blob], 'application.apk', { type: 'application/vnd.android.package-archive' });
+
+//     const downloadLink = document.createElement('a');
+//     downloadLink.href = URL.createObjectURL(file);
+//     downloadLink.download = 'application.apk';
+
+//     document.body.appendChild(downloadLink);
+//     downloadLink.click();
+
+//     document.body.removeChild(downloadLink);
+//   })
+//   .catch(error => {
+//     console.error('Error during fetch operation:', error);
+//   });
+// }
+// apkDownloadBtn.addEventListener("click", downloadApk)
